643fc83611d73120fd0144cf378f9c42d5f32096
deganegane <67388477+deganegane@users.noreply.github.com>
2026-09-15 08:39:55 +0200

    Delete Wdgt directory

--- Wdgt/app/src/main/java/com/dokraja/widget/DoKrajaWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.dokraja.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class DoKrajaWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = DoKrajaWidget()
-}
